f68e4023f38f8ede0e6016f139ca650e61b59479
jpotechin <josephpotechin@gmail.com>
2021-04-12 04:16:31 -0700

    Header created

--- components/AccountContainer.tsx
+++ components/AccountContainer.tsx
@@ -6,6 +6,11 @@ const AccountContainer = (): JSX.Element => {
 			<div className={`${styles.accountContainer}`}>
 				<div className={`${styles.topContainer}`}>
 					<div className={`${styles.backdrop}`}></div>
+					<div className={`${styles.headerContainer}`}>
+						<h2 className={`${styles.headerText}`}>Welcome</h2>
+						<h2 className={`${styles.headerText}`}>Back</h2>
+						<h5 className={`${styles.smallText}`}>Sign in to continue</h5>
+					</div>
 				</div>
 			</div>
 		</div>
